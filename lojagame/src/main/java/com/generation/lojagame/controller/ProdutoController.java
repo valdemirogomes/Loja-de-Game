afbd3d008e65a3548b632d83b3d4a67b0c02eb3a
valdemirogomes <37006124+valdemirogomes@users.noreply.github.com>
2022-12-07 17:19:28 -0300

    Add files via upload

--- lojagame/src/main/java/com/generation/lojagame/controller/ProdutoController.java
+++ lojagame/src/main/java/com/generation/lojagame/controller/ProdutoController.java
@@ -2,21 +2,24 @@ package com.generation.lojagame.controller;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.generation.lojagame.entity.Produto;
-import com.generation.lojagame.repository.ProdutoRepository;
 import com.generation.lojagame.service.ProdutoService;
 
 @RestController
 @RequestMapping("/produto")
 public class ProdutoController {
-	@Autowired
-	private ProdutoRepository repository;
+
 	@Autowired
 	private ProdutoService service;
 
@@ -29,10 +32,28 @@ public class ProdutoController {
 	public ResponseEntity<Produto> getById(@PathVariable Long id) {
 		return service.getById(id);
 	}
-	
+
 	@GetMapping("/titulo/{titulo}")
 	public ResponseEntity<List<Produto>> getByTitulo(@PathVariable String titulo) {
 		return service.getByTitulo(titulo);
+	}
+
+	@PostMapping
+	public ResponseEntity<Produto> post(@RequestBody Produto produto) {
+		return service.post(produto);
+
+	}
+	@PutMapping
+	public ResponseEntity<Produto> put(Produto produto) {
+		return service.update(produto);
+		
+	}
+	
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable Long id) {
+		service.delete(id);
+		
 	}
 
 }
--- lojagame/src/main/java/com/generation/lojagame/service/ProdutoService.java
+++ lojagame/src/main/java/com/generation/lojagame/service/ProdutoService.java
@@ -1,9 +1,14 @@
 package com.generation.lojagame.service;
 
 import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.server.ResponseStatusException;
 import com.generation.lojagame.entity.Produto;
 import com.generation.lojagame.repository.ProdutoRepository;
 
@@ -28,14 +33,35 @@ public class ProdutoService {
 		}
 
 	}
-	
-	public ResponseEntity<List<Produto>> getByTitulo(String titulo){
+
+	public ResponseEntity<List<Produto>> getByTitulo(String titulo) {
 		List<Produto> produto = repository.getByTitulo(titulo);
 		if (produto.isEmpty()) {
 			return ResponseEntity.notFound().build();
 
-		}else {
-		return ResponseEntity.ok(produto);
-		
-	}}
+		} else {
+			return ResponseEntity.ok(produto);
+
+		}
+	}
+
+	public ResponseEntity<Produto> post(@RequestBody Produto produto) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(produto));
+
+	}
+
+	public ResponseEntity<Produto> update(@RequestBody Produto produto) {
+		return repository.findById(produto.getId())
+				.map(response -> ResponseEntity.status(HttpStatus.OK).body(repository.save(produto)))
+				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+
+	}
+
+	public void delete(@PathVariable Long id) {
+		Optional<Produto> postagem = repository.findById(id);
+		if (postagem.isEmpty())
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+		repository.deleteById(id);
+
+	}
 }
